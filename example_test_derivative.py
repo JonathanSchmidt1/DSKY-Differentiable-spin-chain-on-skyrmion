9b852e268b3dc00aad1a41c7edd1ac84e7114515
JonathanSchmidt1 <schmidt.jonathan138@googlemail.com>
2021-11-18 13:37:07 +0100

    example script to test derivative

---
+++ example_test_derivative.py
@@ -0,0 +1,18 @@
+import torch
+
+opt_param_example = torch.rand(200).requires_grad_()
+#set requires grad for parameters that have to be optimized
+
+other_param =  torch.rand(200)
+
+
+#use newly added function here
+def example_function_to_be_tested(opt_param_, other_param_):
+    return  opt_param_+other_param_
+
+
+#create scale loss function by summming
+loss = torch.sum(example_function_to_be_tested(opt_param_example, other_param))
+
+#try to calculate derivative with respect to parameter and check that it is non_zero
+print(torch.sum(torch.abs(torch.autograd.grad(loss, opt_param_example)[0]))>0)
