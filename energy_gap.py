1d33a128438c28c219ed373502007c53cbe35e18
ma-melz <marius.melz@student.uni-halle.de>
2022-04-14 16:58:54 +0200

    added rest of files

--- energy_gap.py
+++ energy_gap.py
@@ -68,6 +68,113 @@ def energy_gap(param_values, J_1 = -1.0, L = 20, n_eigs = 4, dtype = torch.float
     return np.log((eigvals[1] - eigvals[0]).cpu().detach().numpy() + precision)
 
 
+def energy_gap_3d(param_values, scalfac = 1.0, J_1 = -1.0, L = 20, n_eigs = 4, dtype = torch.float32, hdf5_path = "data/sweep/sweep3d"):
+    pathlib.Path(hdf5_path).mkdir(parents=True, exist_ok=True)
+
+    center  = L / 2 - 0.5
+    delta   = param_values[2]
+    B_0     = tensor([param_values[0]], dtype = dtype, device = dev)
+    B_ext   = tensor([param_values[1]], dtype = dtype, device = dev)
+    phi_i   = tensor(Sky_phi(L, center, delta, scalfac), dtype = dtype, device = dev)
+
+    if B_0.dtype == float64:
+        H = ham_total(L, J_1 , B_0, B_ext, phi_i, prec = 64)
+    else:
+        H = ham_total(L, J_1 , B_0, B_ext, phi_i, prec = 32)
+
+    H = H.cpu()
+    H_linop = CsrLinOp(stack([H.storage._row, H.storage._col], dim = 0), H.storage._value, H.size(0), device = "cuda:0")
+
+    torch.cuda.empty_cache()
+
+    def matvec(v, dummy):
+        return H_linop._mv(v)
+
+    initial_state = torch.tensor([1.0]*(2**L), dtype = dtype).random_().cuda()
+    initial_state = initial_state / initial_state.norm()
+
+    num_krylov_vecs = 20
+    numeig = n_eigs
+    which = "SA"
+    tol = 1e-4
+    maxiter = 1000
+    precision = 1e-6
+    
+    eigvals,_,_ = linalg.implicitly_restarted_lanczos_method(matvec, None, initial_state, num_krylov_vecs, numeig, which, tol, maxiter, precision, device = dev)
+    
+    h5file = h5py.File(hdf5_path + "eigvals_{}.hdf5".format(os.environ["CUDA_VISIBLE_DEVICES"]), "a")
+    group = h5file.require_group("eigenvalues")
+
+    data = [param_values, eigvals.cpu().detach().numpy()]
+    if "eigval_data" in group.keys():
+        dset_data = group["eigval_data"][()]
+        dset_data = np.append(dset_data, [data], axis = 0)
+        del group["eigval_data"]
+        group.create_dataset("eigval_data", data = dset_data)
+    else:
+        dset = group.create_dataset("eigval_data", data = [data])
+    
+    h5file.close()
+
+    return np.log((eigvals[1] - eigvals[0]).cpu().detach().numpy() + precision)
+
+
+def energy_gap_test(param_values, J_1 = -1.0, L = 20, n_eigs = 4, dtype = torch.float32, hdf5_path = "data/testing/"):
+    pathlib.Path(hdf5_path).mkdir(parents=True, exist_ok=True)
+
+    center  = L / 2 - 0.5
+    delta   = param_values[2]
+    scalfac = param_values[3]
+    B_0     = tensor([param_values[0]], dtype = dtype, device = dev)
+    B_ext   = tensor([param_values[1]], dtype = dtype, device = dev)
+    phi_i   = tensor(Sky_phi(L, center, delta, scalfac), dtype = dtype, device = dev)
+
+    if B_0.dtype == float64:
+        H = ham_total(L, J_1 , B_0, B_ext, phi_i, prec = 64)
+    else:
+        H = ham_total(L, J_1 , B_0, B_ext, phi_i, prec = 32)
+
+    H = H.cpu()
+    H_linop = CsrLinOp(stack([H.storage._row, H.storage._col], dim = 0), H.storage._value, H.size(0), device = "cuda:0")
+
+    torch.cuda.empty_cache()
+    #eigvals = linalg.symeig(H_linop, neig = n_eigs, method = "davidson", max_niter = 1000, nguess = None,
+    #                        v_init = "randn", max_addition = None, min_eps = 1e-07, verbose = False,
+    #                        bck_options={'method': 'bicgstab', 'rtol': 1e-05, 'atol': 1e-06, 'eps': 1e-8,
+    #                                     'verbose': False, 'max_niter': 100})[0]
+
+    def matvec(v, dummy):
+        return H_linop._mv(v)
+
+    initial_state = torch.tensor([1.0]*(2**L), dtype = dtype).random_().cuda()
+    initial_state = initial_state / initial_state.norm()
+
+    num_krylov_vecs = 20
+    numeig = n_eigs
+    which = "SA"
+    tol = 1e-4
+    maxiter = 1000
+    precision = 1e-6
+    
+    eigvals,_,_ = linalg.implicitly_restarted_lanczos_method(matvec, None, initial_state, num_krylov_vecs, numeig, which, tol, maxiter, precision, device = dev)
+    
+    h5file = h5py.File(hdf5_path + "eigvals_{}.hdf5".format(os.environ["CUDA_VISIBLE_DEVICES"]), "a")
+    group = h5file.require_group("eigenvalues")
+
+    data = [param_values, eigvals.cpu().detach().numpy()]
+    if "eigval_data" in group.keys():
+        dset_data = group["eigval_data"][()]
+        dset_data = np.append(dset_data, [data], axis = 0)
+        del group["eigval_data"]
+        group.create_dataset("eigval_data", data = dset_data)
+    else:
+        dset = group.create_dataset("eigval_data", data = [data])
+    
+    h5file.close()
+
+    return np.log((eigvals[1] - eigvals[0]).cpu().detach().numpy() + precision)
+
+
 
 #@memory_profiler.profile
 def energy_gap_profiling(param_values, J_1 = -1.0, L = 16, n_eigs = 3, dtype = float64):#, scalfac = 1.0, delta = 0.5):
